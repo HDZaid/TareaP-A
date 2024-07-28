b002e6343368ec169b00693cc320d691d41e4cc0
Henry Díaz <99771358+HDZaid@users.noreply.github.com>
2024-07-28 14:49:09 -0600

    Segunda Tarea

---
+++ FuncionesPOO/FuncionesPOO/Calculadora.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//principio de responsabilidad unica
+namespace CalculadoraEstadisticaPOO
+{
+    //es una clase que imita una aplicaicon
+    //puede recibir distintas listas
+    internal class Calculadora
+    {
+        static double resultado = 0;
+        public double Media(List<double> listaNumeros)
+        {
+            double sumaNumeros = 0;
+            foreach (int i in listaNumeros)
+            {
+                sumaNumeros += i;
+            }
+            double resultado = (sumaNumeros / listaNumeros.Count);
+            return resultado;
+        }
+
+        public double Mediana(List<double> listaNumeros)
+        {
+            listaNumeros.Sort();
+            int puntoMedio = listaNumeros.Count / 2;
+
+            if (listaNumeros.Count % 2 == 0)
+            {
+                resultado = (listaNumeros[puntoMedio] + listaNumeros[puntoMedio - 1]) / 2;
+                return resultado;
+            }
+            else
+            {
+                return resultado = listaNumeros[puntoMedio];
+            }
+        }
+
+        public void Moda(List<double> listaNumeros)
+        {
+            Dictionary<double, int> frecuencias = new Dictionary<double, int>();
+
+            foreach (double num in listaNumeros)
+            {
+                if (frecuencias.ContainsKey(num))
+                {
+                    frecuencias[num]++;
+                }
+                else
+                {
+                    frecuencias[num] = 1;
+                }
+            }
+
+            int maxFrecuencia = frecuencias.Values.Max();
+            double moda = frecuencias.First(x => x.Value == maxFrecuencia).Key;
+
+            Console.WriteLine("La moda es: " + moda);
+        }
+
+        public double DesviacionEstandar(List<double> listaNumeros)
+        {
+            double media = Media(listaNumeros);
+            double sumaDesviaciones = 0;
+            foreach (int i in listaNumeros)
+            {
+                double diferencia = i - media;
+                sumaDesviaciones += Math.Pow(diferencia, 2);
+            }
+            double varianza = (sumaDesviaciones / listaNumeros.Count);
+            return resultado = Math.Round(Math.Sqrt(varianza), 6);
+        }
+    }
+}
---
+++ FuncionesPOO/FuncionesPOO/Program.cs
@@ -0,0 +1,148 @@
+﻿using CalculadoraEstadisticaPOO;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+public class Program
+{
+    static List<double> ListaNumeros = new List<double>();
+    static int numeroEntero;
+    static double numero;
+    static double resultado;
+    public static void Main(string[] args)
+    {
+        Calculadora calculadora = new Calculadora();
+        int opcion;
+        do
+        {
+            ImprimirMenu();
+            switch (opcion = LlenarNumeroEntero())
+            {
+                case 1:
+                    Console.Clear();
+                    Console.WriteLine("[1]..... Ingresar numeros a la lista");
+                    LlenarLista();
+                    ImprimirLista();
+                    break;
+                case 2:
+                    Console.Clear();
+                    Console.WriteLine("[2] Calcular la media");
+                    ImprimirLista();
+                    resultado = calculadora.Media(ListaNumeros);
+                    Console.WriteLine("El resultado de la media es: " + resultado);
+                    Console.WriteLine();
+                    break;
+                case 3:
+                    Console.Clear();
+                    Console.WriteLine("[3] Calcular la mediana");
+                    ImprimirLista();
+                    resultado = calculadora.Mediana(ListaNumeros);
+                    Console.WriteLine("La mediana para la lista es: " + resultado);
+                    Console.WriteLine();
+                    break;
+                case 4:
+                    Console.Clear();
+                    Console.WriteLine("[4] Calcular la moda");
+                    ImprimirLista();
+                    calculadora.Moda(ListaNumeros);
+                    Console.WriteLine();
+                    break;
+                case 5:
+                    Console.Clear();
+                    Console.WriteLine("[5] Calcular la desviacion estandar");
+                    ImprimirLista();
+                    calculadora.DesviacionEstandar(ListaNumeros);
+                    Console.WriteLine("La desviacion estandar es: " + resultado);
+                    Console.WriteLine();
+                    break;
+                default:
+                    Console.Clear();
+                    Console.WriteLine("[!] Opcion Invalida");
+                    break;
+            }
+
+        } while (opcion != 6);
+        Console.WriteLine("Justo Geovanny Alcon Mendoza, 1578324");
+        Console.ReadLine();
+    }
+    static void ImprimirMenu()
+    {
+        Console.WriteLine("Calculadora Estadistica");
+        Console.WriteLine("[1] Ingresar numeros a la lista");
+        Console.WriteLine("[2] Calcular la media");
+        Console.WriteLine("[3] Calcular la mediana");
+        Console.WriteLine("[4] Calcular la moda");
+        Console.WriteLine("[5] Calcular la desviacion estandar");
+        Console.WriteLine("[6] Finalizar programa");
+        Console.Write("Ingrese una opcion: ");
+    }
+    static void ImprimirLista()
+    {
+        Console.WriteLine("Lista de numeros: ");
+        Console.Write("\t[  ");
+        foreach (var i in ListaNumeros)
+        {
+            Console.Write(i + "  ");
+        }
+        Console.Write("]\n\n");
+    }
+    static void LlenarLista()
+    {
+        ListaNumeros.Clear();
+        Console.Write("Ingrese el tamaño de los datos que desea llenar: ");
+        int tamaño = LlenarNumeroEntero();
+        for (int i = 0; i < tamaño; i++)
+        {
+            Console.Write($"Ingrese el numero en la posicion ({i + 1}): ");
+            ListaNumeros.Add(LlenarNumero());
+        }
+        Console.WriteLine();
+    }
+    static double LlenarNumero()
+    {//Llenar numero
+        bool valido = false;
+        while (!valido)
+        {
+            try
+            {
+                numero = Convert.ToDouble(Console.ReadLine());
+                //se ejecuta linea por linea si hay una excepcion se captura antes del bool
+                valido = true;
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("[!] Error no puede ingresar letras...");
+                Console.Write("Intente de nuevo: ");
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("[!] Error desconocido... ");
+                Console.Write("> Intente de nuevo: ");
+            }
+        }
+        return numero;
+    }
+    static int LlenarNumeroEntero()
+    {//Llenar numero
+        bool valido = false;
+        while (!valido)
+        {
+            try
+            {
+                numeroEntero = Convert.ToInt32(Console.ReadLine());
+                //se ejecuta linea por linea si hay una excepcion se captura antes del bool
+                valido = true;
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("[!] Error no puede ingresar letras...");
+                Console.Write("Intente de nuevo: ");
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("[!] Error desconocido... ");
+                Console.Write("> Intente de nuevo: ");
+            }
+        }
+        return numeroEntero;
+    }
+
+}
