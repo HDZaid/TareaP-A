64f0cf35e28ce762fb337ae1cca60827ca7f012f
Henry Díaz <99771358+HDZaid@users.noreply.github.com>
2024-07-31 09:59:42 -0600

    recetario

---
+++ RECETAS/RECETAS/Class1.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RECETAS
+{
+    internal class Receta
+    {
+        public List<string> Nombre { get; set; }
+        public List<int> Numero { get; set; }
+        public List<string> Ingredientes { get; set; }
+
+        public Receta()
+        {
+            Nombre = new List<string>();
+            Numero = new List<int>();
+            this.Ingredientes = new List<string>();
+        }
+
+        public void AgregarReceta(String nombre, int numero, String ingredientes)
+        {
+            Nombre.Add(nombre);
+            Numero.Add(numero);
+            Ingredientes.Add(ingredientes);
+            Console.WriteLine("LISTO AGREGADO");
+        }
+        public void MostrarReceta()
+        {
+            Console.WriteLine("LA LISTA DE RECETAS ES:");
+            for(int i = 0; i < Nombre.Count; i++)
+            {
+                Console.WriteLine($"\t\t{Nombre[i]}");
+                Console.WriteLine($"\t\t{Numero[i]}");
+                Console.WriteLine($"\t\t {Ingredientes[i]}");
+            }
+        }
+        public int BuscarReceta()
+        {
+            string nombreBuscar = "";
+            Console.WriteLine("INGRESE EL NOMBRE DE LA RECETA QUE BUSCA");
+            nombreBuscar = Console.ReadLine();
+
+            for (int i = 0; i < Nombre.Count; i++) 
+            {
+                if (Nombre[i] == nombreBuscar)
+                {
+                    return i;
+                    
+                }
+
+            }
+            return -1;
+        }
+
+        public void MostrarRecetaBuscada(int posicion)
+        {
+            Console.WriteLine("LA RECETA ES:");
+            Console.WriteLine($"{Nombre[posicion]}");
+            Console.WriteLine($"{Numero[posicion]}");
+            Console.WriteLine($"{Ingredientes[posicion]}");
+        }
+    }
+    
+
+}
---
+++ RECETAS/RECETAS/Program.cs
@@ -0,0 +1,52 @@
+﻿using RECETAS;
+
+Console.WriteLine("RECETARIO");
+
+bool repetirMenu = true;
+int opcionSwitch = 0;
+Receta LibroRecetas = new Receta();
+while (repetirMenu)
+{
+    MostrarMenu();
+    opcionSwitch=Convert.ToInt32(Console.ReadLine());
+    switch (opcionSwitch)
+    {
+        case 2:
+            
+            string nombre = "";
+            int numero = 0;
+            string descripcion = "";
+            Console.WriteLine("AGREGAR RECETA");
+            Console.WriteLine("NOMBRE");
+            nombre =Console.ReadLine();
+            Console.WriteLine("NUMERO DE LA RECETA");
+            numero = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("DESCRIPCION");
+            descripcion = Console.ReadLine();
+            LibroRecetas.AgregarReceta(nombre, numero,descripcion);
+            break;
+
+        case 3:
+            Console.WriteLine("BUSCAR RECETA");
+            LibroRecetas.MostrarRecetaBuscada(LibroRecetas.BuscarReceta());
+            break;
+
+        case 4:
+            Console.WriteLine("MOSTRAR RECETA");
+            LibroRecetas.MostrarReceta();
+            break; 
+        
+        case 7:
+            Console.WriteLine("SALIR");
+            break;
+    }
+}
+
+static void MostrarMenu()
+{
+    Console.WriteLine("[ 2 ] Agregar receta");
+    Console.WriteLine("[ 3 ] BUSCAR receta");
+    Console.WriteLine("[ 4 ] mostrar receta");
+    Console.WriteLine("[ 7 ] SALIR");
+}
+
